cbbc022fbdb8e8c730241b895ac50d294f4686ef
janaSunrise <warriordefenderz@gmail.com>
2021-04-13 19:43:54 +0530

    Added color class.

---
+++ quick_sort/colors.py
@@ -0,0 +1,11 @@
+class Colors:
+    RED = 255, 114, 111
+    GREEN = 0, 255, 0
+    BLUE = 0, 0, 255
+    YELLOW = 255, 255, 0
+    WHITE = 255, 255, 255
+    BLACK = 0, 0, 0
+    PURPLE = 161, 3, 252
+    ORANGE = 255, 165, 0
+    GREY = 128, 128, 128
+    TURQUOISE = 64, 224, 208
